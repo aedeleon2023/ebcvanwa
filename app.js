da100699ef7176c0977c595a9facccdfc0bda50a
Angela DeLeon <aedeleon2023@gmail.com>
2022-06-27 15:59:05 -0700

    tweaked new code

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let username = window.prompt("What's your name? ");
 alert("Jesus loves you, " + username);
 
-let affirm;
+let text;
 if (confirm("Do you love Jesus?  Press okay if yes.") == true) {
-  affirm = "We love Him because He first loved us. - 1 John 4:19";
+  text = "We love Him because He first loved us. - 1 John 4:19";
 } else {
-  affirm = "Whoever will deny Me before men, him will I also deny before My Father who is in heaven. - Matthew 10:33";
+  text = "Whoever will deny Me before men, him will I also deny before My Father who is in heaven. - Matthew 10:33";
 }
