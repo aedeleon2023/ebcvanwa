d4b8503252cf2e21683b90dc01bd90c194ad9f70
Angela DeLeon <aedeleon2023@gmail.com>
2022-06-26 21:51:52 -0700

    trying out js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+let destination = "Where will you spend eternity, in heaven or hell?";
+if (destination === "heaven") {
+    console.log("So, you have been born again?")
+}'Else'
+{
+    console.log("You're not going to like it!")
+}
+document.write("So, you have been born again?");
