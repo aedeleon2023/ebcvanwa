6eaffd20570714590fc55dc5c935a195556b6470
Angela DeLeon <aedeleon2023@gmail.com>
2022-06-26 22:19:20 -0700

    erased stupid console js stuff, tried to remove underlines from links

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
 'use strict'
 
-let username = prompt('What is your name?');
-console.log('Hello, ' + username )(' How can we pray for you?');
-
-let time = prompt('What time is it?');
-let message;
-
-if(time <= 11){
-    message = 'Good Morning!';
-} else if (time <= 18){
-    message = 'Good Afternoon!';
-}  else if(time <= 24){
-    message = "Good Evening!";
-} else {
-    message = "Time does not exist!";
-}
