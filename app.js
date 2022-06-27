3cf2ed6300eb064524d7db7c0dde07c1160bb358
Angela DeLeon <aedeleon2023@gmail.com>
2022-06-26 22:03:04 -0700

    trying something

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-let destination = "Where will you spend eternity, in heaven or hell?";
-if (destination === "heaven") {
-    console.log("So, you have been born again?")
-}'Else'
-{
-    console.log("You're not going to like it!")
+'use strict'
+
+let username = prompt('What is your name?');
+console.log('Hello, ' + username )(' How can we pray for you?');
+
+let time = prompt('What time is it?');
+let message;
+
+if(time <= 11){
+    message = 'Good Morning!';
+} else if (time <= 18){
+    message = 'Good Afternoon!';
+}  else if(time <= 24){
+    message = "Good Evening!";
+} else {
+    message = "Time does not exist!";
 }
-document.write("So, you have been born again?");
