d77c8a73e7a648fe452dbb6f69cf510d3b3f34fd
Angela DeLeon <aedeleon2023@gmail.com>
2022-06-27 15:55:38 -0700

    added new js ui, tweaked other.

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var username = window.prompt("What's your name? ");
+let username = window.prompt("What's your name? ");
 alert("Jesus loves you, " + username);
+
+let affirm;
+if (confirm("Do you love Jesus?  Press okay if yes.") == true) {
+  affirm = "We love Him because He first loved us. - 1 John 4:19";
+} else {
+  affirm = "Whoever will deny Me before men, him will I also deny before My Father who is in heaven. - Matthew 10:33";
+}
