733065010f6d992152ea7a9726ca31df5d10a303
Angela DeLeon <aedeleon2023@gmail.com>
2022-06-27 22:21:30 -0700

    messed with function, it won.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-let username = window.prompt("What's your name? ");
-alert("Jesus loves you, " + username);
-
-let text;
-if (confirm("Do you love Jesus?  Press okay if yes.") == true) {
-  text = "We love Him because He first loved us. - 1 John 4:19";
-} else {
-  text = "Whoever will deny Me before men, him will I also deny before My Father who is in heaven. - Matthew 10:33";
+"use strict";
+function christianSpam() {
+    let username = window.prompt("What's your name? ");
+    alert("Jesus loves you, " + username);
+    let text;
+    if (confirm("Do you love Jesus?  Press okay if yes.") == true) {
+        text = "We love Him because He first loved us. - 1 John 4:19   ";
+    } else {
+        text = "Whoever will deny Me before men, him will I also deny before My Father who is in heaven. - Matthew 10:33   ";
+    }
+    alert(text + username + ", hope to see you at church this Sunday!");
 }
+
+christianSpam();
+
+
+
+
